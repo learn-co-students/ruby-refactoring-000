62a73018e42706d360a0240ff7edfd341b8aa5c5
mbrickenworthe <adamwinters456@gmail.com>
2015-07-19 16:16:03 -0400

    Refactoring a simple OO class

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Owner
-  # code your solution here!
+
+  attr_accessor :name, :dog, :walking
+  
+  def initialize(name)
+    @name = name
+    @walking = false
+  end
+
+
+  def walk
+    prepare_items
+    dog.walking = true
+  end
+
+  def vet_checkup
+    prepare_items
+    dog.vet_checkup = true
+  end
+
+  def walking?
+    walking
+  end
+
+  def prepare_items
+    dog.leash = true
+    dog.plastic_bag = true
+  end
+
 end
