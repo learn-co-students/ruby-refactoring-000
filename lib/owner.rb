942d0576d3d50fdc1e331f6b6085e90e7e2f70cf
garettarrowood <garettarrowood@gmail.com>
2015-07-23 12:45:35 -0400

    still working

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
-  # code your solution here!
+
+	attr_accessor :name, :dog
+  
+  def initialize(name, dog)
+  	@name = name
+  	@dog = dog
+  end
+
+  def prepare_items
+  	@dog.leash = true
+  	@dog.plastic_bag = true
+  end
+
+  def vet_checkup
+  	prepare_items
+  	@dog.vet = true
+  end
+
+  def walk
+  	prepare_items
+  	@dog.walking = true
+  end
+
+  def walking?
+    @dog.walking?
+  end
+
+  def vet_checkup?(dog)
+    dog.vet_checkup?
+  end
+
 end
