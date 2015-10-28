fd4f147f5afab487607ef3af9e9e939564491024
NoneOfMaster <rcraigt@gmail.com>
2015-10-28 12:17:41 -0700

    funny walking dogs pun

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 class Owner
-  # code your solution here!
+  
+  attr_accessor :dog
+  attr_reader :owner, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def prepare_items
+    self.dog.leash = true
+    self.dog.plastic_bag = true
+  end
+
+  def walk
+    self.prepare_items
+    self.dog.walking = true
+  end
+
+  def vet_checkup
+    self.prepare_items
+    self.dog.vet_checkup = true
+  end
+
 end
