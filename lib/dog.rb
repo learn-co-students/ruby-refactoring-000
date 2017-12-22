165f040b82c7eb8a998aafc0b4086ecdc599041a
iwilliams83 <indirajamurath@gmail.com>
2017-12-22 16:12:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,13 @@
+require_relative './owner.rb'
+
 class Dog
   attr_accessor :leash, :plastic_bag, :walking, :vet
 
   def initialize(name, breed, owner_name)
     @name = name
     @breed = breed
-    @owner = owner_name
+    @owner = Owner.new(owner_name)
+    @owner.dog = self
     @leash = false
     @plastic_bag = false
     @walking = false
@@ -27,20 +30,10 @@ class Dog
     @owner
   end
 
-
   # Refactor the following methods as methods in the Owner class!
 
-  def walk(owner)
-    # REFACTOR!
-    owner.dog.leash = true
-    owner.dog.plastic_bag = true
-    owner.dog.walking = true
+  def vet_checkup
+    @vet_checkup = true
   end
 
-  def vet_checkup(owner)
-    # REFACTOR!
-    owner.dog.leash = true
-    owner.dog.plastic_bag = true
-    owner.dog.vet_checkup = true
-  end
 end
